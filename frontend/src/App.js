dcc8bc3282c200b0122c43343980fe9c36333964
Ryan Li <jijizaza999@gmail.com>
2023-11-30 23:55:58 -0800

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-   <h1>Welcom To Appshop</h1>
+    <>
+  <Header />
+  <main>
+    <Container className='py-3'>
+      <h1>Welcome to AppShop</h1>
+    </Container>
+    </main>
+    <Footer />
+  </>
   )
 }
 
