b7b89aa5efd59facdbba17443de12cc3636c05e4
Ryan Li <jijizaza999@gmail.com>
2024-01-19 19:48:35 -0800

    second commit

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
-
+connectDB();
 
 const app = express();
-connectDB();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
